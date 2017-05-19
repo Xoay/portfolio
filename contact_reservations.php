bbafbab5d2d599ae7491f419d5f9a497ee921ec1
Vinh Luu <vluu87@gmail.com>
2017-05-18 19:24:26 -0500

    php contact testing

--- contact_reservations.php
+++ contact_reservations.php
@@ -1,23 +1,23 @@
 <?php
 if($_POST)
 {
-	$to_Email   	= "support@bestlooker.pro"; //Replace with recipient email address
+	$to_Email   	= "vluu87@gmail.com"; //Replace with recipient email address
 	$subject        = 'Message from website '.$_SERVER['SERVER_NAME']; //Subject line for emails
-	
-	
+
+
 	//check if its an ajax request, exit if not
     if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-	
+
 		//exit script outputting json data
 		$output = json_encode(
 		array(
-			'type'=>'error', 
+			'type'=>'error',
 			'text' => 'Request must come from Ajax'
 		));
-		
+
 		die($output);
-    } 
-	
+    }
+
 	//check $_POST vars are set, exit if any missing
 	if(!isset($_POST["userName"]) || !isset($_POST["userPhone"]))
 	{
@@ -31,28 +31,28 @@ if($_POST)
 	$user_Date        = filter_var($_POST["userDate"], FILTER_SANITIZE_STRING);
 	$user_Time        = filter_var($_POST["userTime"], FILTER_SANITIZE_STRING);
 	$user_Message     = filter_var($_POST["userMessage"], FILTER_SANITIZE_STRING);
-	
+
 	$user_Message = str_replace("\&#39;", "'", $user_Message);
 	$user_Message = str_replace("&#39;", "'", $user_Message);
-	
+
 	//additional php validation
 	if(strlen($user_Name)<4) // If length is less than 4 it will throw an HTTP error.
 	{
 		$output = json_encode(array('type'=>'error', 'text' => 'Name is too short or empty!'));
 		die($output);
 	}
-	
+
 	if(strlen($user_Phone)<4) // If length is less than 4 it will throw an HTTP error.
 	{
 		$output = json_encode(array('type'=>'error', 'text' => 'Phone is too short or empty!'));
 		die($output);
 	}
-	
+
 	//proceed with PHP email.
 	$headers = 'X-Mailer: PHP/' . phpversion();
-	
+
 	$sentMail = @mail($to_Email, $subject, $user_Message . "\r\n\n"  .'Name: '.$user_Name. "\r\n" .'Phone: '.$user_Phone. "\r\n" .'Date: '.$user_Date. "\r\n" .'Time: '.$user_Time, $headers);
-	
+
 	if(!$sentMail)
 	{
 		$output = json_encode(array('type'=>'error', 'text' => 'Could not send mail! Please check your PHP mail configuration.'));
